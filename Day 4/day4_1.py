78a7fafe54fc773c786f337f8e431fcce612dd10
anton <antonjollant@aol.com>
2022-12-16 21:03:45 +0100

    Day 4 slimmed down

--- Day 4/day4_1.py
+++ Day 4/day4_1.py
@@ -3,45 +3,24 @@ frauds = 0
 with open('Day 4/input.txt') as f:
     lines = f.readlines()
 
-instructions = []
-instructions2 = []
+for line in lines:
+    pair = line.split(',')
+    pretty_first = pair[0].split('-')   # ['6', '90']
+    pretty_second = pair[1].split('-')  # ['91', '91']
+    print(pretty_first, pretty_second)
+    
+    set_1 = set()
+    set_2 = set()
 
-# for i in lines:
-#     pretty_i = i.split(',')
-#     for j in pretty_i:
-#         pretty_j = j.split('-')
-
-#     print(pretty_i[1])
-
-
-# str1 = 28 -- 88
-
-# str2 = 34 -- 43
-
-# if (str2[0] >= str1[0] and str2[1] <= str1[1]) or (str2[0] >= str1[0] and str2[1] <= str1[1]):
-#     print('fraud!')
-#     frauds += 1
+    for i in range(int(pretty_first[0]), int(pretty_first[1])+1):
+        set_1.add(i)
+    for i in range(int(pretty_second[0]), int(pretty_second[1])+1):
+        set_2.add(i)
 
 
-
-# set_1 = set()
-# set_2 = set()
-# instructions = [43, 43]
-# instructions2 = [34, 43]
-
-
-
-# for i in range(int(instructions[0]), int(instructions[1])+1):
-#     set_1.add(i)
-# for i in range(int(instructions2[0]), int(instructions2[1])+1):
-#     set_2.add(i)
-
-
-# if set_1.difference(set_2) and set_2.difference(set_1):
-#     print('worth')
-# else:
-#     print('fraud')
-#     frauds += 1 
-
-
-
+    if set_1.difference(set_2) and set_2.difference(set_1):
+        print('worth it')
+    else:
+        print('fraud!')
+        frauds += 1 
+    print(frauds)
